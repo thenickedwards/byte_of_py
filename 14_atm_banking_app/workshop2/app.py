359c6eb734a18a0d51f65e72be6cdff536015a1a
Nick Edwards <nickedwards.dev@gmail.com>
2024-11-03 13:37:41 -0800

    auth v2 functionality with json, updates needed to deposit and balance

--- 14_atm_banking_app/workshop2/app.py
+++ 14_atm_banking_app/workshop2/app.py
@@ -73,6 +73,54 @@ def login():
             print("\n   Invalid Credentials.\n  We cannot verify the name and PIN. Please try to login again.")
             continue
 
+### AUTHENTICATION v2
+def login2():
+    global name
+    global pin
+    print("\n          === Automated Teller Machine ===          ")
+    while True:
+        login_name = input("Enter name to login: ")
+        login_pin = input("Enter PIN to login: ")
+        
+        with open('14_atm_banking_app/workshop2/user_db.json') as users:
+            user_db = json.load(users)
+            if login_name in user_db and user_db[login_name]['pin'] == login_pin:
+                print(f"\nWelcome {name}! \nYour have successfully logged in.\n\n")
+                break
+            elif login_name not in user_db:
+                print("\nWe cannot find that username in the database. \nPlease reigster first to login.")
+                continue
+            else:
+                print(f"\nInvalid username or password.  \nPlease verify your username and password or reigster first to login.\n\n")
+                continue
+
+
+def register2():
+    global name
+    global pin
+    print("\n          === Automated Teller Machine ===          ")
+    while name == "" or pin == "":
+        name = input("Enter name to register: ")
+        pin = input("Enter PIN to register: ")
+        if name == "" or len(name) > 10:
+            print("ERROR\nInvalid name to register. Please register a name for login. Note: names must be 10 characters or less.")
+            print("\nLet's start over with registration.\n")
+            continue
+        elif len(pin) != 4 or not re.match(r"\d\d\d\d", pin):    # Negation Logic - could also be written elif len(pin) == 4 and re.match("\d\d\d\d", pin):
+            print("ERROR \nInvalid PIN to register. Please register a 4 digit PIN for login.")
+            pin = ""
+            print("\n   Let's start over with registration.\n")
+            continue
+        else:
+            with open('14_atm_banking_app/workshop2/user_db.json', 'r') as users:
+                user_db = json.load(users)
+            new_user = { name: { "pin": pin, "balance": 0.00 } }
+            user_db.update(new_user)
+            with open('14_atm_banking_app/workshop2/user_db.json', 'w') as updated_db:
+                updated_db.write(json.dumps(user_db))
+            print(f"Thank you, {name}! Your name and pin have been registered for login.")
+            print(f"Hello {name}, your account shows a starting balance of ${balance:.2f}.")
+            break
 
 
 ##### MAIN APPLICATION BELOW #####
@@ -87,13 +135,10 @@ def main_atm():
         start_menu()            # display start menu
         start_choice = input("Enter 1 to register or 2 to login: ")
         if start_choice == "1":
-            register()
+            register2()
             break
         if start_choice == "2":
-            if name == "" or pin == "":
-                print("\nWe encountered an unexpected error. \n\nThanks for your patience. Please try again.")
-                continue
-            login()
+            login2()
             break
         else:
             print("\n   Sorry, I can't understand that choice.\n  Please choose to register or login.")
