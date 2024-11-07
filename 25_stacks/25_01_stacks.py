3e5bca9f93504d0f01cee8ab4e586635249b5609
Nick Edwards <nickedwards.dev@gmail.com>
2024-11-06 22:14:55 -0800

    Stacks algos

---
+++ 25_stacks/25_01_stacks.py
@@ -0,0 +1,15 @@
+'''
+Stacks
+Order of items in Stack is based on the order in which they were added (e.g. LIFO: Last In, First Out).
+
+All stacks have 2 methods:
+* push() - adds item to top of stack
+* pop() - removes item from top of stack
+
+push↘___↗pop
+    |_4_| ← top
+    |_3_|
+    |_2_|
+    |_1_|
+    
+'''
