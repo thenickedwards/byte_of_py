2b1d49715b7e0db2514c5528dc34051b54293b20
Nick Edwards <nickedwards.dev@gmail.com>
2024-11-06 22:33:40 -0800

    Queues algos

--- 25_stacks/25_01_stacks.py
+++ 25_stacks/25_01_stacks.py
@@ -1,6 +1,6 @@
 '''
 Stacks
-Order of items in Stack is based on the order in which they were added (e.g. LIFO: Last In, First Out).
+Order of items in Stack is based on the order in which they were added (LIFO: Last In, First Out).
 
 All stacks have 2 methods:
 * push() - adds item to top of stack
---
+++ 26_queues/25_01_queues.py
@@ -0,0 +1,17 @@
+'''
+Queues
+Order of items in Queue is based on the order in which they were added (FIFO: First In, First Out).
+
+All queues have 2 methods:
+
+enqueue() - adds item to one end of sequence
+← 1 ←
+← 1 2 ←
+← 1 2 3 ←
+
+dequeue() - removes item from other end of sequence
+← 1 2 3 ←
+← 2 3 ←
+← 3 ←
+
+'''
