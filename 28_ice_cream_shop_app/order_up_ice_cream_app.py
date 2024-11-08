dbd33693b044b5ba6d96c8d8d2c7d99cf3bff5c1
Nick Edwards <nickedwards.dev@gmail.com>
2024-11-08 10:22:20 -0800

    working on enqueuing

--- 28_ice_cream_shop_app/order_up_ice_cream_app.py
+++ 28_ice_cream_shop_app/order_up_ice_cream_app.py
@@ -24,20 +24,17 @@ class IceCreamShop:
         self.orders = Queue()
         
     def take_order(self, customer, flavor, scoops):
-        # verify flavor available
-        if flavor not in self.flavors:
-            raise ValueError("Sorry, we don't have that flavor.")
-            # print("Sorry, we don't have that flavor.")
-        # validate scoops
         try:
+            # verify flavor available
+            if flavor not in self.flavors:
+                raise ValueError("Sorry, we don't have that flavor.")
+            # validate scoops
             scoops = int(scoops)
+            if scoops < 1 or scoops > 3:
+                raise ValueError("Sorry, I can only take an order for 1, 2, or 3 scoops.")
         except ValueError:
             raise ValueError("Please enter a valid number of scoops.")
-            # print("Please enter a valid number of scoops.")
-        if scoops < 1 or scoops > 3:
-            raise ValueError("Sorry, I can only take an order for 1, 2, or 3 scoops.")
-            # print("Sorry, I can only take an order for 1, 2, or 3 scoops.")
-        # enqueue order
+        # enqueue order if no errors in try/except
         else:
             print('Order created!')
     
