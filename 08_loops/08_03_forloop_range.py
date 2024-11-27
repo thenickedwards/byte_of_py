381e865113b90e54d2439e3ce44d9c2923bb5ac9
Nick Edwards <nickedwards.dev@gmail.com>
2024-11-27 12:06:25 -0800

    Missed one

--- 08_loops/08_03_forloop_range.py
+++ 08_loops/08_03_forloop_range.py
@@ -2,7 +2,7 @@
 # for x in range(0, 10, 2):     # output: 0 - 8 by twos     then "For loop has finished"
 # for x in range(-5, 10, 2):    # output: -5 - 9 by twos    then "For loop has finished"
 # for x in range(-5, -10, -1):  # output: -5 - 9            then "For loop has finished"
-for x in range(1, 1):
+for x in range(1, 1):           # NO output
     print(x)
 print("For loop has finished")  
 
